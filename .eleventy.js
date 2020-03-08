13f5eb503aef7073364fcc76257ddd7dbfd60829
Dave Powers <djpowers89@gmail.com>
2020-03-08 19:35:52 -0400

    Add favicon
    - created using Favicon.io
    - https://favicon.io/favicon-generator/?t=D&ff=Fira+Mono&fs=110&fc=%23FFFFFF&b=rounded&bc=midnightblue

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addPassthroughCopy("_redirects");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
   return {
     passthroughFileCopy: true
   };
