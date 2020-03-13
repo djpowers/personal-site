cbd4896bd2620ce3f7690fbc5f8d5caf2c799037
Dave Powers <djpowers89@gmail.com>
2020-03-13 12:56:16 -0400

    Add local images eleventy plugin
    - grabs remote assets and deploys them locally
    - using default settings

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,13 @@
+const localImages = require("eleventy-plugin-local-images");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(localImages, {
+    distPath: "_site",
+    assetPath: "/assets/img",
+    selector: "img",
+    verbose: false
+  });
+
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addPassthroughCopy("_redirects");
